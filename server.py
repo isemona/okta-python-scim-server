028fdf2631eaeb4b1b54ff6ad07d404b6395a0f4
Semona <semona.igama@gmail.com>
2021-11-15 14:33:23 -0800

    first commit

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import os
+from re import U
 from flask import Flask, flash, redirect, request, render_template, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from models import User
+#from models import User
 
 app = Flask(__name__)
 database_url = os.getenv('DATABASE_URL', 'postgresql:///test-users.db')
@@ -13,33 +14,6 @@ db = SQLAlchemy(app)
 def hello():
     """Homepage"""
     return render_template('index.html')
-
-
-@app.route("/scim/v2/Users", methods=['GET'])
-def users_get():
-    """Get Users"""    
-    query = User.query
-    request_filter = request.args.get('filter')
-    match = None
-    if request_filter:
-        match = re.match('(\w+) eq "([^"]*)"', request_filter)
-    if match:
-        (search_key_name, search_value) = match.groups()
-        search_key = getattr(Users, search_key_name)
-        query = query.filter(search_key == search_value)
-    count = int(request.args.get('count', 100))
-    start_index = int(request.args.get('startIndex', 1))
-    if start_index < 1:
-        start_index = 1
-    start_index -= 1
-    query = query.offset(start_index).limit(count)
-    total_results = query.count()
-    found = query.all()
-    response = ListResponse(found,
-                      start_index=start_index,
-                      count=count,
-                      total_results=total_results)
-    return flask.jsonify(response.to_scim_resource())
 
 if __name__ == "__main__":
     app.run(debug=True)
